978300ef2f9832949e532509625dde781892d851
1552971348 <1552971348@qq.com>
2019-07-11 20:29:47 +0800

    获取串口列表，并显示在下拉选择框和文本框中

--- EasyCom.py
+++ EasyCom.py
@@ -14,11 +14,11 @@ window.title('My Window')
 
 
 # 第3步，设定窗口的大小(长 * 宽)
-window.geometry('500x300')  # 这里的乘是小x
+window.geometry('500x500')  # 这里的乘是小x
 
 # 第4步，在图形界面上设定输入框控件entry框并放置
 e = tk.Entry(window, show=None)  # 显示成明文形式
-e.pack()
+e.grid(column=0, row=0)
 
 
 # 第5步，定义两个触发事件时的函数insert_point和insert_end（注意：因为Python的执行顺序是从上往下，所以函数一定要放在按钮的上面）
@@ -37,6 +37,12 @@ def insert_point():  # 在鼠标焦点处插入输入内容
             var = port_list[i]
             t.insert('insert', var)
             t.insert('insert', '\n')
+        numberChosen.textvariable = len(port_list)
+        list_portname = []
+        for eachPort in port_list:
+            list_portname.append(eachPort.device)
+        numberChosen['values'] = list_portname  # 设置下拉列表的值
+        numberChosen.current(0)  # 设置下拉列表默认显示的值，0为 numberChosen['values'] 的下标值
 
 
 def insert_end():  # 在文本框内容最后接着插入输入内容
@@ -45,18 +51,25 @@ def insert_end():  # 在文本框内容最后接着插入输入内容
 
 
 # 第6步，创建并放置两个按钮分别触发两种情况
-b1 = tk.Button(window, text='insert point', width=10,
+b1 = tk.Button(window, text='Get Serial', width=10,
                height=2, command=insert_point)
-b1.pack()
+b1.grid(column=0, row=1)
 b2 = tk.Button(window, text='insert end', width=10,
                height=2, command=insert_end)
-b2.pack()
+b2.grid(column=0, row=2)
 
 
 
 # 第7步，创建并放置一个多行文本框text用以显示，指定height=3为文本框是三个字符高度
 t = tk.Text(window, width=50,height=10)
-t.pack()
+t.grid(column=0, row=3)
+
+
+
+numberChosen = ttk.Combobox(window, width=22, textvariable=5)
+numberChosen['values'] = (1, 2, 4, 42, 100)  # 设置下拉列表的值
+numberChosen.grid(column=0, row=4)  # 设置其在界面中出现的位置  column代表列   row 代表行
+
 
 
 # 第8步，主窗口循环显示
