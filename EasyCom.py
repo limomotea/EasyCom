0cebbe0bf9d44b557d61e84da3d3ae73fca71f6a
1552971348 <1552971348@qq.com>
2019-07-12 12:55:06 +0800

    串口打开 发送接收一个字节

--- EasyCom.py
+++ EasyCom.py
@@ -46,8 +46,27 @@ def insert_point():  # 在鼠标焦点处插入输入内容
 
 
 def insert_end():  # 在文本框内容最后接着插入输入内容
-    var = e.get()
-    t.insert('end', var)
+    try:
+        port_list = list(serial.tools.list_ports.comports())
+        portx = port_list[0].device
+        bps = 115200
+        # 超时设置,None：永远等待操作，0为立即返回请求结果，其他值为等待超时时间(单位为秒）
+        timex = None
+        ser = serial.Serial(portx, bps, timeout=timex)
+        print("串口详情参数：", ser)
+
+        # 十六进制的发送
+        result = ser.write(chr(0x06).encode("utf-8"))  # 写数据
+        print("Test", result)
+
+        # 十六进制的读取
+        print(ser.read().hex())  # 读一个字节
+
+        print("---------------")
+        ser.close()  # 关闭串口
+
+    except Exception as e:
+        print("---异常---：", e)
 
 
 # 第6步，创建并放置两个按钮分别触发两种情况
